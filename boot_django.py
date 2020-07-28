ec73328521c9a18d5279e6415597e9ad590bc5b9
Vincent Nys <vincentnys@gmail.com>
2020-07-28 13:50:56 +0200

    Copied from RealPython, getting a different error than before.

---
+++ boot_django.py
@@ -0,0 +1,17 @@
+# boot_django.py
+#
+# This file sets up and configures Django. It's used by scripts that need to
+# execute as if running in a Django server.
+import os
+import django
+from django.conf import settings
+
+def boot_django():
+    settings.configure(
+        DEBUG=True,
+        INSTALLED_APPS=(
+            "xchk_regex_strategies",
+        ),
+        AUTH_USER_MODEL="django.contrib.auth.models.User"
+    )
+    django.setup()
