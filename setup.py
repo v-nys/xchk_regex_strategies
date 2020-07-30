c0d3aa703dcc0971cba124a6f1336161a1c2e8ea
Vincent Nys <vincentnys@gmail.com>
2020-07-30 17:18:21 +0200

    Going to try adding setup.py so that I can use git repo instead of PyPI, should be faster and more convenient to have editable dependencies.

---
+++ setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['xchk_regex_strategies']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['regex>=2020.7.14,<2021.0.0', 'xchk-core>=0.1.0']
+
+setup_kwargs = {
+    'name': 'xchk-regex-strategies',
+    'version': '0.1.1',
+    'description': 'Checking strategies for xchk based around regular expressions.',
+    'long_description': None,
+    'author': 'Vincent Nys',
+    'author_email': 'vincentnys@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
