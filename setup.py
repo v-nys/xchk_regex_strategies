c44e22cd05c95ffdccfdcdfe9f6e20b5e723b345
Vincent Nys <vincentnys@gmail.com>
2020-07-30 17:34:50 +0200

    setup.py aangevuld om xchk-core op git te gebruiken

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ package_data = \
 {'': ['*']}
 
 install_requires = \
-['regex>=2020.7.14,<2021.0.0', 'xchk-core>=0.1.0']
+['regex>=2020.7.14,<2021.0.0', 'xchk-core>=0.1.6']
 
 setup_kwargs = {
     'name': 'xchk-regex-strategies',
@@ -23,6 +23,7 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
+    'dependency_links'=['http://github.com/v-nys/xchk_core/tarball/master#egg=xchk-core-1.0.6'],
     'python_requires': '>=3.7,<4.0',
 }
 
