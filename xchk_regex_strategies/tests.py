9acb64496fbdac1637a7c2d5f87b17b0c9ac1a08
Vincent Nys <vincentnys@gmail.com>
2020-08-20 12:15:03 +0200

    update

--- xchk_regex_strategies/tests.py
+++ xchk_regex_strategies/tests.py
@@ -5,7 +5,7 @@ from unittest.mock import MagicMock
 from django.test import TestCase
 from xchk_regex_strategies.strats import RegexCheck
 from xchk_core.strats import OutcomeAnalysis, OutcomeComponent
-from xchk_core.models import SubmissionV2
+from xchk_core.models import Submission
 
 # tests go here
 class RegexMatchingTest(TestCase):
@@ -17,7 +17,7 @@ class RegexMatchingTest(TestCase):
         # file handle's read method is called to produce text
         mock_open = MagicMock()
         mock_open.return_value.__enter__.return_value.read.return_value = 'acdddddddde'
-        submission = SubmissionV2(content_uid='some_uid')
+        submission = Submission(content_uid='some_uid')
         analysis = chk.check_submission(submission=submission,student_path='/student',desired_outcome=True,init_check_number=1,ancestor_has_alternatives=False,parent_is_negation=False,open=mock_open)
         expected = OutcomeAnalysis(outcome=True,outcomes_components=[OutcomeComponent(component_number=1,outcome=True,desired_outcome=True,rendered_data='',acceptable_to_ancestor=True)])
         self.assertEquals(analysis,expected)
@@ -26,7 +26,7 @@ class RegexMatchingTest(TestCase):
         chk = RegexCheck(pattern=regex.compile(r'a[bc]d+e'))
         mock_open = MagicMock()
         mock_open.return_value.__enter__.return_value.read.return_value = 'acddddddddf'
-        submission = SubmissionV2(content_uid='some_uid')
+        submission = Submission(content_uid='some_uid')
         analysis = chk.check_submission(submission=submission,student_path='/student',desired_outcome=True,init_check_number=1,ancestor_has_alternatives=False,parent_is_negation=False,open=mock_open)
         expected = OutcomeAnalysis(outcome=False,outcomes_components=[OutcomeComponent(component_number=1,outcome=False,desired_outcome=True,rendered_data='<p>Je oplossing verschilt van een gekend patroon vanaf regel 1, karakter 11.</p>',acceptable_to_ancestor=False)])
         self.assertEquals(analysis,expected)
@@ -35,7 +35,7 @@ class RegexMatchingTest(TestCase):
         chk = RegexCheck(pattern=regex.compile(r'a[bc]d+e\nfghi'))
         mock_open = MagicMock()
         mock_open.return_value.__enter__.return_value.read.return_value = 'acdddddddde\nfgih'
-        submission = SubmissionV2(content_uid='some_uid')
+        submission = Submission(content_uid='some_uid')
         analysis = chk.check_submission(submission=submission,student_path='/student',desired_outcome=True,init_check_number=1,ancestor_has_alternatives=False,parent_is_negation=False,open=mock_open)
         expected = OutcomeAnalysis(outcome=False,outcomes_components=[OutcomeComponent(component_number=1,outcome=False,desired_outcome=True,rendered_data='<p>Je oplossing verschilt van een gekend patroon vanaf regel 2, karakter 3.</p>',acceptable_to_ancestor=False)])
         self.assertEquals(analysis,expected)
